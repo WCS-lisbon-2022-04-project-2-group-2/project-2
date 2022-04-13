c3604b59ec28731178f64e0ea8eb64a56b02d649
santilieb <santiago.liebrecht@gmail.com>
2022-04-13 15:51:49 +0100

    Added attempts to fetch character information

--- src/components/Pokedex/Characters/Characters.jsx
+++ src/components/Pokedex/Characters/Characters.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-
+import React, {useEffect} from "react";
+import axios from "axios"
 
 function Characters(props){
-    const { name} = props;
+
+
+    const getIndividualCharacters = () => {
+       axios
+            .get({url})
+            .then(response => console.log(response.data))
+    }
+    
+    
+        getIndividualCharacters()
+    
+
+    const { name , url} = props;
     return(
-        <div className="card">
+        <div className="card" id={name}>
             <h2>{name}</h2>
+            <img src='' alt={`pokemon ${name}`}/>
         </div>
     )
 };
--- src/components/Pokedex/CharactersList/CharactersList.jsx
+++ src/components/Pokedex/CharactersList/CharactersList.jsx
@@ -1,23 +1,50 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Characters from '../Characters/Characters';
 
 function CharactersList(){
+    //create the array
     const [characters, setCharacters] = useState([])
+    const [individual, setIndividual] = useState('')
+    const [allPokemon, setAllPokemon] = useState([])
 
+
+    //create module to fetch data from API
     const getCharacter = () => {
+
         axios
         .get('https://pokeapi.co/api/v2/pokemon/')
         .then(response => setCharacters(response.data.results))
+
+            // //for every element obtain the name
+            // //for every name obtain the URL for all the pokemon info
+            // axios
+            // .get(`https://pokeapi.co/api/v2/pokemon/${e.name}`)
+            // .then(response => array.push(response.data))
+            // //prints the "data" from outside of the map scope
         
-        console.log("mounted again and again")
     }
-    useEffect(() => {
-        getCharacter()
-    }, [])
+    
+    // console.log(characters)
+
+    // const getInformation = (event) => {
+    //     event.preventDefault()
+    //     setIndividual(event.target.id)
+
+    //     axios
+    //     .get(`https://pokeapi.co/api/v2/pokemon-form/${individual}`)
+    //     .then(response => console.log(response.data.sprites.front_default))
+    // }
+        // characters.indexOf()
+
+        // axios
+        // .get('https://pokeapi.co/api/v2/pokemon/')
+        // .then(response => setCharacters(response.data.results))
+    
+  
     
     //in the following line "characters" is a State, specifically an array
-    const displayCharacters = characters.map(character => <Characters {...character}/>)
+    const displayCharacters = characters.map(character => <Characters {...character} key={character.name}/>)
 
     return(
         <div>
