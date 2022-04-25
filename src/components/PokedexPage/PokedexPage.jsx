ffef30becefbc435ab3311505ff6daa015ca737f
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-04-25 21:30:44 +0100

    update css project

--- src/components/PokedexPage/PokedexPage.jsx
+++ src/components/PokedexPage/PokedexPage.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import './PokedexPage.css'
-import pokedexImage from '../../media/images/pokedex.png'
+import { Link } from "react-router-dom";
+import "./PokedexPage.css";
+import pokedexImage from "../../media/images/pokedex.png";
 
 function PokedexPage() {
-    return (
-        <div className="pokedex-page-main-container">
-            <div className="cardPokedexPage">
-                <Link to="/pokedex-page/pokedex">
-                    <h1>Pokédex</h1>
-                    <img src={pokedexImage} alt="pokedex section"/>
-                </Link>
-            </div>
-            <div className="cardPokedexPage">
-                <Link to="/pokedex-page/my-pokemon">
-                    <h1>My Pokémon</h1>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="my captured pokemon section" />
-                </Link>
-            </div>
-        </div>
-      );
-    
+  return (
+    <div className="pokedex-page-main-container">
+      <div className="cardPokedexPage">
+        <Link to="/pokedex-page/pokedex">
+          <h1 className="pokedex-title">Pokédex</h1>
+          <img
+            className="pokedex-img"
+            src={pokedexImage}
+            alt="pokedex section"
+          />
+        </Link>
+      </div>
+      <div className="cardPokedexPage">
+        <Link to="/pokedex-page/my-pokemon">
+          <h1 className="pokedex-title">My Pokémon</h1>
+          <img
+            className="pikatchu-img"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+            alt="my captured pokemon section"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default PokedexPage;
