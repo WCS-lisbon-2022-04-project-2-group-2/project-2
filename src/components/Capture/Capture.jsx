c8303fe0d89f52ebb9b63b5bcddcc109c35f23a6
Nuno Moniz <nuno-moniz_student2022@wilder.school>
2022-04-28 11:47:19 +0100

    show image and name of catched pokemon and buttons to decide

--- src/components/Capture/Capture.jsx
+++ src/components/Capture/Capture.jsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from "react-router-dom"
+import useFetch from '../../hooks/useFetch';
+import "./Capture.css";
+
+import GameContext from '../../contexts/GameContext';
 
 
-function capture() {
+export default function Capture() {
+
+    const { wildPokemon } = useContext(GameContext)
+
+        // Para testes de CSS
+        const url = "https://pokeapi.co/api/v2/pokemon/1/";
+
+        // correcta
+        // const url = `https://pokeapi.co/api/v2/pokemon/${wildPokemon}/`;
+
+        const { isLoading, error, response } = useFetch(url);
+        
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+        
+        if (error) {
+            return <div>Oops...something went wrong...</div>;
+        }
+        
+        const name = response.name.toUpperCase();
+
+        const capturePokemon = () => {
+            localStorage.setItem(wildPokemon, name)
+        }
+
     return (
         <div>
-            <h1 className="catch-pokemon_title">{} fainted!</h1>
-            <img className="catch_pokemon" src={"#"} alt="Fainted Pokemon" />
-
+            <h1 className="catch-pokemon_title">
+                {name} fainted!
+            </h1>
+            <img className="catch_pokemon"
+            src={response.sprites.other["official-artwork"].front_default}
+            alt="Fainted Pokemon" 
+            />
+            <div>
+            <Link to="/game/captured">
+                <button onClick={()=> capturePokemon()} className="btn-catch">
+                    Catch
+                </button>
+            </Link>
             
-            <button className="btn-catch">Catch</button>
+            <Link to="/game/choose-generation">
+                <button className='btn-discard'>
+                    Discard
+                </button>
+            </Link>
+            </div>
         </div>
     );
 }
-
-export default capture;
