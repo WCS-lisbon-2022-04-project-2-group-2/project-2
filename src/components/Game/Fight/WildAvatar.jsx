5a936e77e551378777752e056e01be923668d829
santilieb <santiago.liebrecht@gmail.com>
2022-04-28 21:42:27 +0100

    Added WildBox and WildAvatar components

---
+++ src/components/Game/Fight/WildAvatar.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import GameContext from "../../../contexts/GameContext";
+
+function WildAvatar(){
+    const { wildFaint, wildPokemon } = useContext(GameContext)
+    const wildImage = wildPokemon[0].sprites.front_default;
+    let animation;
+    if (wildFaint === true){
+        animation = "animated fadeOut slow";
+    } else if (wildFaint === false) {
+        animation = "animated zoomIn slow";
+    } else if (wildFaint === ""){
+        animation = "hide";
+    }
+    return (
+      <div className={animation}>
+        <img className="avatar mr-3 mt-4" src={wildImage} alt="" />
+      </div>
+    );
+}
+
+export default WildAvatar;
