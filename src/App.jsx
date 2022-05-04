50c851287b350c94bcbeebbfe0292731746f9a44
santilieb <santiago.liebrecht@gmail.com>
2022-05-04 16:17:46 +0100

    Added healthbar, still does not decrease or change color when HP decrease

--- src/App.jsx
+++ src/App.jsx
@@ -38,23 +38,11 @@ function App() {
             <Route path="/about" element={<About />}></Route>
             <Route path="/pokedex-page" element={<PokedexPage />}></Route>
             <Route path="/pokedex-page/pokedex" element={<Pokedex />}></Route>
-            <Route
-              path="/pokedex-page/pokedex/:id"
-              element={<PokemonCard />}
-            ></Route>
-            <Route
-              path="/pokedex-page/my-pokemon"
-              element={<MyPokemon />}
-            ></Route>
+            <Route path="/pokedex-page/pokedex/:id" element={<PokemonCard />}></Route>
+            <Route path="/pokedex-page/my-pokemon"element={<MyPokemon />}></Route>
             <Route path="/" element={<HomePage />}></Route>
-            <Route
-              path="/game/choose-generation"
-              element={<ChooseGeneration />}
-            ></Route>
-            <Route
-              path="/game/choose-starter"
-              element={<ChooseStarter />}
-            ></Route>
+            <Route path="/game/choose-generation" element={<ChooseGeneration />}></Route>
+            <Route path="/game/choose-starter" element={<ChooseStarter />}></Route>
             <Route path="/game/starter-card" element={<StarterCard />}></Route>
             <Route path="/game/choose-region" element={<PokemonRegion />}></Route>
             <Route path="/game/wild-pokemon/:region/:id" element={<WildPokemon />}></Route>
--- src/components/Game/Fight/StarterBox.jsx
+++ src/components/Game/Fight/StarterBox.jsx
@@ -34,30 +34,28 @@ function StarterBox() {
         {/* END HERO POKEMON AVATAR PICTURE */}
 
         {/* HERO POKEMON INFO BOX */}
-        <div id="hero-info-box">
+        <div id="enemy-info-box">
           <div className="d-flex justify-content-between align-items-center">
-            <h2 id="hero-name">{starterName}</h2>
+            <h2 id="enemy-name">{starterHealth}</h2>
           </div>
-          <div className="health-points_container">
-            <div className="d-flex justify-content-between align-items-center ml-3 mr-1">
-              <h5>HP</h5>
-              <div className="progress ml-1 both-progress">
-                <div
-                  className={progressColor}
-                  role="progressbar"
-                  style={{ width: percentage }}
-                  aria-valuenow="75"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                />
-              </div>
+          <div className="d-flex justify-content-between align-items-center ml-3 mr-1">
+            <h5>HP</h5>
+            <div className="progress ml-1 both-progress">
+              <div
+                className={progressColor}
+                role="progressbar"
+                style={{ width: percentage }}
+                aria-valuenow="75"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              />
             </div>
-            <div id="hero-hp" className="d-flex">
-              <div className="ml-auto mr-3">
-                <h5>
-                  {starterOverKill ? `0/200` :`${starterHealth}/200`}
-                </h5>
-              </div>
+          </div>
+          <div id="hero-hp" className="d-flex">
+            <div className="ml-auto mr-3">
+              <h5>
+                {starterHealth}/200
+              </h5>
             </div>
           </div>
         </div>
@@ -69,3 +67,4 @@ function StarterBox() {
 }
 
 export default StarterBox;
+
